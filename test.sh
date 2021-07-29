637f7d68d73c1567383076a340ed68838d783e53
Franck Pachot (YB) <33070466+FranckPachot@users.noreply.github.com>
2021-07-29 18:44:14 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
-# change the following to your database. This will drop and create tables starting with 'bench':
-export PGHOST=yb1.pachot.net PGPORT=5433 PGUSER=franck PGDATABASE=yugabyte PGPASSWORD=PleaseDontBreakMyDB
 # create setup() and runit() procedures, and the benchruns table:
 psql -e ybio.sql
 # create two tables:
 for i in {1..1} ; do psql <<<"call setup(tab_prefix=>'bench',tab_num=>${i},tab_rows=>1e6::int);" ; done ; wait
 #
 for i in {1..1} ; do psql <<<"call runit(tab_prefix=>'bench',tab_num=>${i},tab_rows=>1e6::int,run_duration=>interval '1 minutes',pct_update=>5);" & done ; wait
-# you can look at http://${PGHOST}:7000/tablet-servers to watch Read/Write ops/sec
+
+# for YugabyteDB you can look at http://${PGHOST}:7000/tablet-servers to watch Read/Write ops/sec
 
 # check results:
 psql <<'SQL'
