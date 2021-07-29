79b8c0fe6d1f378bb3be37c9ab516ade2d2992cc
Franck Pachot (YB) <33070466+FranckPachot@users.noreply.github.com>
2021-07-29 18:28:23 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ psql -e ybio.sql
 # create two tables:
 for i in {1..2} ; do psql <<<"call setup(tab_prefix=>'bench',tab_num=>${i},tab_rows=>1e6::int);" ; done ; wait
 #
-for i in {1..2} ; do psql <<<"call runit(tab_prefix=>'bench',tab_num=>${i},tab_rows=>1e6::int,run_duration=>interval '1 minutes',pct_update=>5);" ; done ; wait
+for i in {1..2} ; do psql <<<"call runit(tab_prefix=>'bench',tab_num=>${i},tab_rows=>1e6::int,run_duration=>interval '1 minutes',pct_update=>5);" & done ; wait
 # you can look at http://${PGHOST}:7000/tablet-servers to watch Read/Write ops/sec
