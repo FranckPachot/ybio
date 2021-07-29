5dfe43df06ed8e560547616df5ef43b5249b35d2
Franck Pachot (YB) <33070466+FranckPachot@users.noreply.github.com>
2021-07-29 18:40:34 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,16 @@ export PGHOST=yb1.pachot.net PGPORT=5433 PGUSER=franck PGDATABASE=yugabyte PGPAS
 # create setup() and runit() procedures, and the benchruns table:
 psql -e ybio.sql
 # create two tables:
-for i in {1..2} ; do psql <<<"call setup(tab_prefix=>'bench',tab_num=>${i},tab_rows=>1e6::int);" ; done ; wait
+for i in {1..1} ; do psql <<<"call setup(tab_prefix=>'bench',tab_num=>${i},tab_rows=>1e6::int);" ; done ; wait
 #
-for i in {1..2} ; do psql <<<"call runit(tab_prefix=>'bench',tab_num=>${i},tab_rows=>1e6::int,run_duration=>interval '1 minutes',pct_update=>5);" & done ; wait
+for i in {1..1} ; do psql <<<"call runit(tab_prefix=>'bench',tab_num=>${i},tab_rows=>1e6::int,run_duration=>interval '1 minutes',pct_update=>5);" & done ; wait
 # you can look at http://${PGHOST}:7000/tablet-servers to watch Read/Write ops/sec
+
+# check results:
+psql <<'SQL'
+select end_time-start_time duration,round(num_rows/extract(epoch from end_time-start_time)) riops
+,round(100*max_scratch::float/table_scratch) as pct_scratch
+, case when num_rows > table_rows then lpad(to_char(num_rows::float/table_rows,'xfmB9990D9'),6) 
+  else lpad(to_char(100*num_rows/table_rows,'fmB999 %'),6) end coverage
+,* from benchruns order by job_id desc nulls last limit 10;
+SQL
