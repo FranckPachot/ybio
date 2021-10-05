910cb0eb7409fefd09e72d868b3a0d6035532bea
Franck Pachot (YB) <33070466+FranckPachot@users.noreply.github.com>
2021-10-05 09:41:51 +0200

    Update quick-flame.sh

--- util_flamegraph/quick-flame.sh
+++ util_flamegraph/quick-flame.sh
@@ -8,3 +8,4 @@ wget -c https://raw.githubusercontent.com/FranckPachot/ybio/main/util_flamegraph
 sudo perf script -i perf.data | FlameGraph/stackcollapse-perf.pl | FlameGraph/flamegraph.pl --colors green --width=1200 --hash --cp | tee perf.svg && chmod a+r perf.svg
 # here is how to share it though internet:
 curl --upload-file perf.svg http://transfer.sh/$(date +%Y%m%dT%H%M%S).svg
+the url displayed can be opened publicly
